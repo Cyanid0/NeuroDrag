4aad457225ec191266b4cc40c82a56fb1183d604
Adam Dhayfa Umar <adam@usedatabrain.com>
2024-05-15 12:50:45 +0530

    feat: dump

--- src/components/navBar/navbar.tsx
+++ src/components/navBar/navbar.tsx
@@ -4,14 +4,17 @@ type navBarProp = {
   isOpen: boolean,
   setIsOpen: (x: boolean) => void,
   showGenerate: boolean,
+  handleGenerate: () => void,
 }
-function NavBar({ isOpen, setIsOpen, showGenerate }: navBarProp) {
+function NavBar({ isOpen, setIsOpen, showGenerate, handleGenerate }: navBarProp) {
 
   return (
     <div className="flex sticky top-0 border-black border-b-2 items-center justify-between bg-white w-[100%] h-20">
       <MenuIcon isOpen={isOpen} setIsOpen={setIsOpen} />
       {showGenerate &&
-        <div className="flex items-center justify-center w-[13%] h-[80%] text-md text-black border-black rounded-lg border-md border-2 mr-2 hover:text-white hover:bg-black hover:cursor-pointer transition-all duration-200"> Generate Code </div>
+        <div className="flex items-center justify-center w-[13%] h-[80%] text-md text-black border-black rounded-lg border-md border-2 mr-2 hover:text-white hover:bg-black hover:cursor-pointer transition-all duration-200"
+          onClick={handleGenerate}
+        > Generate Code </div>
 
       }
     </div>
